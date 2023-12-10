8bc7d16f00d6a93b44e0db5bf57a63f47c5ffcfc
ramonsalcedo <ramonisalcedo@hotmail.com>
2023-12-10 09:28:12 -0400

    Initial Commit

---
+++ holamundo2/holamundo2App.swift
@@ -0,0 +1,17 @@
+//
+//  holamundo2App.swift
+//  holamundo2
+//
+//  Created by ramon salcedo on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct holamundo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
